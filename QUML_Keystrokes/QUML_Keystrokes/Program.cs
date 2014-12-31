e67f739580d618a859c9de9aa04207d33aacf1d8
Pranav Krishnamurthy <pkrishna@nyit.edu>
2014-12-31 17:18:38 -0500

    First commit of my own keystroke analyzer (non-classwork)

---
+++ QUML_Keystrokes/QUML_Keystrokes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+///<summary>
+/// Pranav S. Krishnamurthy
+/// 31st December 2014
+/// Version: 0.1
+/// Purpose: To review my knowledge that I learned about using various verifiers
+/// to be able to analyze data sets, and also to enhance my knowledge about the 
+/// concepts behind machine learning. 
+///</summary>
+
+namespace QUML_Keystrokes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the QUML Analyzer!");
+
+            Console.ReadKey(); 
+        }
+    }
+}
