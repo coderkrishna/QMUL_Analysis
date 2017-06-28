d6f5b95fcd19094cc291499f79edf9875e2d4e39
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-06-28 16:08:51 -0400

    Latest changes

--- QUML_Keystrokes/QUML_Keystrokes/ScoreVars2.cs
+++ QUML_Keystrokes/QUML_Keystrokes/ScoreVars2.cs
@@ -21,6 +21,34 @@ namespace QUML_Keystrokes
             get { return _impostorScoresUser5User2; }
             set { _impostorScoresUser5User2 = value; }
         }
+
+        private double[,] _impostorScoresUser5User3;
+        public double[,] ImpostorScoresUser5User3
+        {
+            get { return _impostorScoresUser5User3; }
+            set { _impostorScoresUser5User3 = value; }
+        }
+
+        private double[,] _impostorScoresUser5User4;
+        public double[,] ImpostorScoresUser5User4
+        {
+            get { return _impostorScoresUser5User4; }
+            set { _impostorScoresUser5User4 = value; }
+        }
+
+        private double[,] _impostorScoresUser5User6;
+        public double[,] ImpostorScoresUser5User6
+        {
+            get { return _impostorScoresUser5User6; }
+            set { _impostorScoresUser5User6 = value; }
+        }
+
+        private double[,] _impostorScoresUser5User7;
+        public double[,] ImpostorScoresUser5User7
+        {
+            get { return _impostorScoresUser5User7; }
+            set { _impostorScoresUser5User7 = value; }
+        }
         #endregion
     }
 }
