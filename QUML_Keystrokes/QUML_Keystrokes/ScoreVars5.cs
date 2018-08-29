9d5b378a050466edcd5f533bc108b6d96853d55d
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-08-29 12:16:10 -0400

    Having the latest changes
    Changing the code around to meet C# 7 practices, and also adding a new partial class to the project

---
+++ QUML_Keystrokes/QUML_Keystrokes/ScoreVars5.cs
@@ -0,0 +1,35 @@
+﻿namespace QUML_Keystrokes
+{
+    public partial class ImpostorScoreVars
+    {
+        #region Impostor scores for User 41
+        private double[,] _impostorScoresUser41User1;
+        public double[,] ImpostorScoresUser41User1
+        {
+            get { return _impostorScoresUser41User1; }
+            set { _impostorScoresUser41User1 = value; }
+        }
+
+        private double[,] _impostorScoresUser41User2;
+        public double[,] ImpostorScoresUser41User2
+        {
+            get { return _impostorScoresUser41User2; }
+            set { _impostorScoresUser41User2 = value; }
+        }
+
+        private double[,] _impostorScoresUser41User3;
+        public double[,] ImpostorScoresUser41User3
+        {
+            get { return _impostorScoresUser41User3; }
+            set { _impostorScoresUser41User3 = value; }
+        }
+
+        private double[,] _impostorScoresUser41User4;
+        public double[,] ImpostorScoresUser41User4
+        {
+            get { return _impostorScoresUser41User4; }
+            set { _impostorScoresUser41User4 = value; }
+        }
+        #endregion
+    }
+}
