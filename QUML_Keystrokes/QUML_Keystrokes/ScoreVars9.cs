b67d08aec6ce1b65972d3be4920ee3d5ec6aec95
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-01-27 16:49:09 -0800

    Updates being made
    Making sure more variables are being created

--- QUML_Keystrokes/QUML_Keystrokes/ScoreVars9.cs
+++ QUML_Keystrokes/QUML_Keystrokes/ScoreVars9.cs
@@ -6,6 +6,29 @@
         public double[,] ImpostorScoresUser81User1 { get; set; }
         public double[,] ImpostorScoresUser81User2 { get; set; }
         public double[,] ImpostorScoresUser81User3 { get; set; }
+        public double[,] ImpostorScoresUser81User4 { get; set; }
+        public double[,] ImpostorScoresUser81User5 { get; set; }
+        public double[,] ImpostorScoresUser81User6 { get; set; }
+        public double[,] ImpostorScoresUser81User7 { get; set; }
+        public double[,] ImpostorScoresUser81User8 { get; set; }
+        public double[,] ImpostorScoresUser81User9 { get; set; }
+        public double[,] ImpostorScoresUser81User10 { get; set; }
+
+        public double[,] ImpostorScoresUser81User11 { get; set; }
+        public double[,] ImpostorScoresUser81User12 { get; set; }
+        public double[,] ImpostorScoresUser81User13 { get; set; }
+        public double[,] ImpostorScoresUser81User14 { get; set; }
+        public double[,] ImpostorScoresUser81User15 { get; set; }
+        public double[,] ImpostorScoresUser81User16 { get; set; }
+        public double[,] ImpostorScoresUser81User17 { get; set; }
+        public double[,] ImpostorScoresUser81User18 { get; set; }
+        public double[,] ImpostorScoresUser81User19 { get; set; }
+        public double[,] ImpostorScoresUser81User20 { get; set; }
+
+        public double[,] ImpostorScoresUser81User21 { get; set; }
+        public double[,] ImpostorScoresUser81User22 { get; set; }
+        public double[,] ImpostorScoresUser81User23 { get; set; }
+        public double[,] ImpostorScoresUser81User24 { get; set; }
         #endregion
     }
 }
